79f28f2b86daec00d082fca63efe5755d9185aa6
yuban10703 <39484884+yuban10703@users.noreply.github.com>
2020-10-08 13:42:29 +0800

    add setgroupAnnounce

--- botoy/action.py
+++ botoy/action.py
@@ -303,6 +303,19 @@ class Action:
             'OidbSvc.0x89a_0',
             {"GroupID": group, "Switch": switch},
         )
+    def setgroupAnnounce(self, group: int, Title: str, Text: str, Pinned: bool, typ: bool):
+    """设置群公告;Title可以空,Pinned 是否置顶 typ:是否发送新成员"""
+    if typ:
+        Type = 20
+    else:
+        Type = 0
+    return self.post(path='/v1/Group/Announce', funcname='', payload={
+        "GroupID": group,
+        "Title": Title,
+        "Text": Text,
+        "Pinned": Pinned,
+        "Type": Type
+    })
 
     ############################################################################
     def baseRequest(
