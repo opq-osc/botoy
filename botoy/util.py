8ed5f9bc4fcabf92cd998921f8f5324b3d1e3747
wongxy <xiyao.wong@foxmail.com>
2021-04-26 20:42:53 +0800

    Add utility function get_cache_dir utils.get_cache_dir 封装对缓存目录的常用方法，可以快速的获取较为统一的缓存目录

--- botoy/util.py
+++ botoy/util.py
@@ -1,8 +1,12 @@
 import base64
+import inspect
+import os
 import re
+from pathlib import Path
 
 
 def file_to_base64(path):
+    """获取文件base64编码"""
     with open(path, 'rb') as f:
         content = f.read()
     return base64.b64encode(content).decode()
@@ -13,3 +17,35 @@ def check_schema(url: str) -> str:
     if not re.findall(r'(http://|https://)', url):
         return "http://" + url
     return url
+
+
+def get_cache_dir(dir_name: str) -> Path:
+    """获取缓存目录
+    :param dir_name: the cache directory's name.
+    :return: A Path object of the cache directory
+    """
+    # 确定主缓存目录
+    for i in os.listdir(Path('.')):
+        if i in ['botoy.json', 'REMOVED_PLUGINS', 'bot.py', 'plugins']:
+            main_dir = Path('.')
+            break
+    else:
+        cf = inspect.currentframe()
+        bf = cf.f_back
+        file = bf.f_globals['__file__']
+        dir_ = Path(file).absolute()
+        while 'plugins' in str(dir_):
+            dir_ = dir_.parent
+            if dir_.name == 'plugins':
+                main_dir = dir_.parent
+                break
+        else:
+            main_dir = Path('.')
+
+    cache_dir = main_dir / 'botoy-cache'
+    this_cache_dir = cache_dir / dir_name
+    if not cache_dir.exists():
+        cache_dir.mkdir()
+    if not this_cache_dir.exists():
+        this_cache_dir.mkdir()
+    return this_cache_dir
