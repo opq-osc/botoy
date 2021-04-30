7566013b27ec902eab7c8c5d6b90a7c5f4cb8063
wongxy <xiyao.wong@foxmail.com>
2021-04-30 17:04:49 +0800

    feat(schedule): 使用apscheduler库提供定时任务功能

---
+++ botoy/schedule.py
@@ -0,0 +1,32 @@
+import logging
+
+from apscheduler.schedulers.background import BackgroundScheduler
+
+from .config import jconfig
+from .log import LoguruHandler, logger
+
+# Reference: https://github.com/nonebot/plugin-apscheduler
+apscheduler_logger = logging.getLogger("apscheduler")
+apscheduler_logger.setLevel(int(jconfig.apscheduler_log_level or 30))
+apscheduler_logger.handlers.clear()
+apscheduler_logger.addHandler(LoguruHandler())
+
+scheduler = BackgroundScheduler()
+
+
+def start_scheduler():
+    if not scheduler.running:
+        scheduler.configure(
+            {
+                "apscheduler.timezone": "Asia/Shanghai",
+                **(jconfig.apscheduler_config or {}),
+            }
+        )
+        scheduler.start()
+        logger.info('定时任务已启动')
+
+
+if jconfig.apscheduler_autostart is None or jconfig.apscheduler_autostart:
+    start_scheduler()
+
+__all__ = ['scheduler', 'start_scheduler']
---
+++ docs/schedule.md
@@ -0,0 +1,33 @@
+# 定时任务
+
+定时任务使用库`apscheduler`实现，对基本配置项进行封装
+
+## 示例
+
+```python
+from botoy.schedule import scheduler
+
+job1 = scheduler.add_job(lambda: print("我一分钟出现一次"), 'interval', minutes=1)
+```
+
+具体使用方式见:[apscheduler adding jobs](https://apscheduler.readthedocs.io/en/latest/userguide.html#adding-jobs)
+
+## 配置
+
+在`botoy.json`进行配置
+
+配置项以及默认值为
+
+```json
+{
+  "apscheduler_autostart": true,
+  "apscheduler_log_level": 30,
+  "apscheduler_config": {
+    "apscheduler.timezone": "Asia/Shanghai"
+  }
+}
+```
+
+- apscheduler_autostart: 是否自动启动定时任务
+- apscheduler_log_level: scheduler 的日志等级, WARNING = 30 INFO = 20
+- apscheduler_config: apscheduler 的配置项，见[configuration_guide](https://apscheduler.readthedocs.io/en/latest/userguide.html#configuring-the-scheduler) [configuration_api](https://apscheduler.readthedocs.io/en/latest/modules/schedulers/base.html#apscheduler.schedulers.base.BaseScheduler.configure)
