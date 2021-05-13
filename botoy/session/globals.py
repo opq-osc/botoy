cc1193c34c5e03d872635a8bdf1959d79f4438c9
wongxy <xiyao.wong@foxmail.com>
2021-05-13 19:23:58 +0800

    :recycle: bind contextvar

--- botoy/session/globals.py
+++ botoy/session/globals.py
@@ -5,16 +5,33 @@ from ..model import FriendMsg, GroupMsg
 from .base import Session
 
 
-class ContextVarProxy:
-    def __init__(self, var):
-        self.var = var
+def bind_contextvar(contextvar):
+    class ContextVarBind:
+        __slots__ = ()
 
-    def __getattr__(self, attr):
-        return getattr(self.var.get(), attr)
+        def __getattr__(self, name):
+            return getattr(contextvar.get(), name)
+
+        def __setattr__(self, name, value):
+            setattr(contextvar.get(), name, value)
+
+        def __delattr__(self, name):
+            delattr(contextvar.get(), name)
+
+        def __getitem__(self, index):
+            return contextvar.get()[index]
+
+        def __setitem__(self, index, value):
+            contextvar.get()[index] = value
+
+        def __delitem__(self, index):
+            del contextvar.get()[index]
+
+    return ContextVarBind()
 
 
-_ctx = ContextVar("ctx")
-_session = ContextVar("session")
+_ctx: ContextVar[Union[FriendMsg, GroupMsg]] = ContextVar("ctx")
+_session: ContextVar[Session] = ContextVar("session")
 
-ctx: Union[FriendMsg, GroupMsg] = ContextVarProxy(_ctx)
-session: Session = ContextVarProxy(_session)
+ctx: Union[FriendMsg, GroupMsg] = bind_contextvar(_ctx)
+session: Session = bind_contextvar(_session)
