cd7384dc9d20ab92e400f5f3e40a401fe23d65f0
wongxy <xiyao.wong@foxmail.com>
2021-06-09 21:56:51 +0800

    :recycle: move bind_contextvar function to utils module

--- botoy/session/globals.py
+++ botoy/session/globals.py
@@ -2,33 +2,8 @@ from contextvars import ContextVar
 from typing import Union
 
 from ..model import FriendMsg, GroupMsg
+from ..utils import bind_contextvar
 from .base import Session
-
-
-def bind_contextvar(contextvar):
-    class ContextVarBind:
-        __slots__ = ()
-
-        def __getattr__(self, name):
-            return getattr(contextvar.get(), name)
-
-        def __setattr__(self, name, value):
-            setattr(contextvar.get(), name, value)
-
-        def __delattr__(self, name):
-            delattr(contextvar.get(), name)
-
-        def __getitem__(self, index):
-            return contextvar.get()[index]
-
-        def __setitem__(self, index, value):
-            contextvar.get()[index] = value
-
-        def __delitem__(self, index):
-            del contextvar.get()[index]
-
-    return ContextVarBind()
-
 
 _ctx: ContextVar[Union[FriendMsg, GroupMsg]] = ContextVar("ctx")
 _session: ContextVar[Session] = ContextVar("session")
