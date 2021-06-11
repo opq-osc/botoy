92e0e561836b312c0c0598bfa863e29c8b0e0ad3
wongxy <xiyao.wong@foxmail.com>
2021-06-11 22:46:55 +0800

    Add decorator: ignore admin

--- botoy/decorators/__init__.py
+++ botoy/decorators/__init__.py
@@ -6,6 +6,7 @@ from ._from_botself import from_botself
 from ._from_phone import from_phone
 from ._from_these_groups import from_these_groups
 from ._from_these_users import from_these_users
+from ._ignore_admin import ignore_admin
 from ._ignore_botself import ignore_botself
 from ._ignore_tempMsg import ignore_tempMsg
 from ._ignore_these_groups import ignore_these_groups
