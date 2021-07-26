4be506b8c15d31cddd7f3e7fa123c7ed0c025837
wongxy <xiyao.wong@foxmail.com>
2021-07-26 11:14:42 +0800

    refactor(queued_up): 改用工作池

--- botoy/decorators/_queued_up.py
+++ botoy/decorators/_queued_up.py
@@ -1,35 +1,18 @@
 import functools
-import time
 from collections import defaultdict
-from queue import Queue
-from threading import Thread
 
-from botoy.log import logger
+from ..pool import WorkerPool
 
 
-class TaskThread(Thread):
+class TaskThread(WorkerPool):
     def __init__(self):
-        super().__init__()
-        self.tasks = Queue(maxsize=-1)
-        self.setDaemon(True)
-        self.start()
-
-    def run(self):
-        while True:
-            try:
-                self.tasks.get()()
-            except Exception as e:
-                logger.warning(f"queued_up装饰器: 队列任务出错{e}")
-            finally:
-                self.tasks.task_done()
-            time.sleep(1)
+        super().__init__(max_workers=1)
 
     def put_task(self, target, *args):
-        task = functools.partial(target, *args)
-        self.tasks.put(task)
+        self.submit(target, *args)
 
 
-taskThread_dict = defaultdict(TaskThread)
+task_pool_dict = defaultdict(TaskThread)
 
 
 def queued_up(func=None, *, name="default"):
@@ -40,7 +23,6 @@ def queued_up(func=None, *, name="default"):
         return functools.partial(queued_up, name=name)
 
     def inner(ctx):
-        task_thread = taskThread_dict[repr(name)]
-        task_thread.put_task(func, ctx)
+        task_pool_dict[repr(name)].put_task(func, ctx)
 
     return inner
