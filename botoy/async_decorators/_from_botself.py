612f05be6fd8d2a594fc909443672cacf97fab2d
wongxy <xiyao.wong@foxmail.com>
2021-08-31 07:45:36 +0800

    async_deco: from_botself

---
+++ botoy/async_decorators/_from_botself.py
@@ -0,0 +1,19 @@
+from ..model import FriendMsg, GroupMsg
+
+
+def from_botself(func=None):
+    """只处理机器人自身的消息 GroupMsg, FriendMsg"""
+    if func is None:
+        return from_botself
+
+    async def inner(ctx):
+        assert isinstance(ctx, (GroupMsg, FriendMsg))
+        if isinstance(ctx, GroupMsg):
+            userid = ctx.FromUserId
+        else:
+            userid = ctx.FromUin
+        if userid == ctx.CurrentQQ:
+            return await func(ctx)
+        return None
+
+    return inner
