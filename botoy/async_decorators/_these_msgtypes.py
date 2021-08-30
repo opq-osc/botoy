844cd209566be0680b79ba0032aa50c6dd432c68
wongxy <xiyao.wong@foxmail.com>
2021-08-31 07:51:12 +0800

    async_deco: these_msgtypes

---
+++ botoy/async_decorators/_these_msgtypes.py
@@ -0,0 +1,18 @@
+from ..model import FriendMsg, GroupMsg
+
+
+def these_msgtypes(*msgtypes):
+    """仅接受该些类型消息  GroupMsg, FriendMsg
+    模块collection中定义了这些消息类型
+    """
+
+    def deco(func):
+        async def inner(ctx):
+            assert isinstance(ctx, (GroupMsg, FriendMsg))
+            if ctx.MsgType in msgtypes:
+                return await func(ctx)
+            return None
+
+        return inner
+
+    return deco
