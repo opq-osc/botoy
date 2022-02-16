1bf5ca4ebbd03f73448fd669183dd127494dbdd8
wongxy <xiyao.wong@foxmail.com>
2022-02-17 00:55:39 +0800

    feat(check_version): 增加版本更新信息的提示

--- botoy/__version__.py
+++ botoy/__version__.py
@@ -1,11 +1,10 @@
 # pylint: disable=C0415,C0413
 # type: ignore
-__version__ = "7.15"
+__version__ = "7.16"
 
 
 def check_version():
     def _check_version():
-        import sys
         from distutils.version import LooseVersion as V
         from xml.etree import ElementTree
 
@@ -26,10 +25,41 @@ def check_version():
         except Exception:
             pass
         else:
+
             local_version = V(__version__)
             if local_version < latest_version:
-                info = f"\n\033[33m==== 当前版本为: \033[31m{local_version}\033[33m, 已有最新版本: \033[31m{latest_version}\033[33m, 请及时更新! ====\033[0m\n"
-                sys.stdout.write(info)
+
+                try:
+                    from rich.console import Console
+                    from rich.markdown import Markdown
+
+                    def mprint(msg):
+                        Console().print(Markdown(msg))
+
+                except Exception:
+                    mprint = print
+
+                info = f"\n\n\033[33m==== 当前版本为: \033[31m{local_version}\033[33m, 已有最新版本: \033[31m{latest_version}\033[33m, 请及时更新! ====\033[0m\n\n"
+
+                try:
+                    releases = httpx.get(
+                        "https://api.github.com/repos/opq-osc/botoy/releases",
+                        timeout=10,
+                    ).json()
+                    needed_releases = []
+                    for release in releases:
+                        if V(release["tag_name"][1:]) > local_version:
+                            needed_releases.append(release)
+
+                    tips = []
+                    for release in needed_releases:
+                        tips.append(f"**{release['tag_name']}**\n{release['body']}\n")
+
+                    info += "\n".join(tips)
+                except Exception:
+                    pass
+
+                mprint(info)
 
     from threading import Thread
 
