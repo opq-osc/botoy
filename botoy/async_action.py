10952efb7e3e66211a824c056fba9b65c117221f
wongxy <xiyao.wong@foxmail.com>
2022-05-22 19:08:38 +0800

    async action

--- botoy/async_action.py
+++ botoy/async_action.py
@@ -830,6 +830,11 @@ class AsyncAction:
         """
         return await self.post("", {"HDIMGUrl": url}, path="/v1/SelfHDIMG")
 
+    async def getAllBots(self) -> List[int]:
+        """获取OPQ登陆的所有机器人QQ号"""
+        clusterInfo = await self.getClusterInfo()
+        return [i["QQ"] for i in clusterInfo["QQUsers"]]
+
     ############################################################################
     async def baseRequest(
         self,
