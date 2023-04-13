55bbae9fd97304e27344dea997ea94a4883bdfb5
wongxy <xiyao.wong@foxmail.com>
2023-04-13 19:54:57 +0800

    receiver_marker: override operator add

--- botoy/_internal/client.py
+++ botoy/_internal/client.py
@@ -1,16 +1,14 @@
 import asyncio
 import importlib
 import inspect
-import logging
 import os
 import re
 import signal
-import sys
 import threading
 import traceback
 from contextvars import copy_context
 from pathlib import Path
-from typing import Optional
+from typing import Callable, List, Tuple, Union
 from urllib.parse import urlparse
 from uuid import uuid4
 
@@ -23,8 +21,74 @@ from .context import Context, ctx_var
 from .log import logger
 from .pool import WorkerPool
 
-connected_clients = []
 
+class ReceiverMarker:
+    def __call__(
+        self, receiver, name="", author="", usage="", *, __directly_attached=False
+    ):
+        """标记接收函数
+        该信息仅用于开发者调试
+        :param receiver: 接收函数
+        :param name: 插件名称，默认为__name__
+        :param author: 插件作者，默认为空
+        :param usage: 插件用法，默认为__doc__
+        """
+        receiver.__dict__["is_receiver"] = True
+        meta = ""
+        if file := inspect.getsourcefile(receiver):
+            meta += str(Path(file).relative_to(os.getcwd()))
+        try:
+            lines = inspect.getsourcelines(receiver)
+            if meta:
+                meta += " line {}".format(lines[1])
+        except:
+            pass
+
+        receiver.__dict__["receiver_info"] = {
+            "author": author or "",
+            "usage": usage or receiver.__doc__ or "",
+            "name": name or receiver.__name__ or "",
+            "meta": meta or "",
+        }
+
+        # 插件通过加入额外信息标记接收函数, 其前提该函数能在import后的module中**被检索**到
+        # 被attach调用时，函数会被直接添加，所以无需进行该操作
+        if not __directly_attached:
+            back_globals = inspect.currentframe().f_back.f_globals  # type: ignore
+            if receiver not in back_globals.values():
+                u = "receiver" + str(uuid4())
+                back_globals[u] = receiver
+        return self
+
+    def __add__(self, receiver: Union[Callable, Tuple, List]):
+        if receiver == self:
+            pass
+        elif callable(receiver):
+            self(receiver)
+        elif isinstance(receiver, (List, Tuple)):
+            items = list(receiver)
+            items.extend(["", "", ""])
+            self(items[0], *items[1:3])
+        else:
+            # TODO ???
+            pass
+        return self
+
+
+mark_recv = ReceiverMarker()
+
+
+def is_recv(receiver):
+    try:
+        return receiver.__dict__.get("is_receiver", False)
+    except Exception:
+        return False
+
+
+#####################
+
+
+connected_clients = []
 is_signal_hander_set = False
 
 
@@ -42,48 +106,6 @@ def async_signal_handler():
         asyncio.get_event_loop().stop()
 
     asyncio.ensure_future(_handler())
-
-
-def mark_recv(receiver, name="", author="", usage="", *, __directly_attached=False):
-    """标记接收函数
-    该信息仅用于开发者调试
-    :param receiver: 接收函数
-    :param name: 插件名称，默认为__name__
-    :param author: 插件作者，默认为空
-    :param usage: 插件用法，默认为__doc__
-    """
-    receiver.__dict__["is_receiver"] = True
-    meta = ""
-    if file := inspect.getsourcefile(receiver):
-        meta += str(Path(file).relative_to(os.getcwd()))
-    try:
-        lines = inspect.getsourcelines(receiver)
-        if meta:
-            meta += " line {}".format(lines[1])
-    except:
-        pass
-
-    receiver.__dict__["receiver_info"] = {
-        "author": author or "",
-        "usage": usage or receiver.__doc__ or "",
-        "name": name or receiver.__name__ or "",
-        "meta": meta or "",
-    }
-
-    # 插件通过加入额外信息标记接收函数, 其前提该函数能在import后的module中**被检索**到
-    # 被attach调用时，函数会被直接添加，所以无需进行该操作
-    if not __directly_attached:
-        back_globals = inspect.currentframe().f_back.f_globals  # type: ignore
-        if receiver not in back_globals.values():
-            u = "receiver" + str(uuid4())
-            back_globals[u] = receiver
-
-
-def is_recv(receiver):
-    try:
-        return receiver.__dict__.get("is_receiver", False)
-    except Exception:
-        return False
 
 
 class Botoy:
