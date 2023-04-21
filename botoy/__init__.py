c5e3bee55096ef50fdd974917c8bdea8661bef34
wongxy <xiyao.wong@foxmail.com>
2023-04-21 23:18:33 +0800

    export logger

--- botoy/__init__.py
+++ botoy/__init__.py
@@ -11,6 +11,7 @@ from ._internal.client import Botoy as Botoy
 from ._internal.client import mark_recv as mark_recv
 from ._internal.config import jconfig as jconfig
 from ._internal.context import ctx as ctx
+from ._internal.log import logger as logger
 from ._internal.schedule import async_scheduler as async_scheduler
 from ._internal.schedule import scheduler as scheduler
 from ._internal.schedule import start_scheduler as start_scheduler
